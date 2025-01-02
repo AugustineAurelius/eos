e39af8a0ed41aaf61c1cbdf20ae397ab5c25fab5
AugustineAurelius <kserbulov@gmail.com>
2025-01-02 03:42:04 +0300

    add test with postgres

--- example/repository/integration_test.go
+++ example/repository/integration_test.go
@@ -2,7 +2,13 @@ package repository_test
 
 import (
 	"context"
+	"log"
 	"testing"
+	"time"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 
 	"github.com/AugustineAurelius/eos/example/common"
 	"github.com/AugustineAurelius/eos/example/repository"
@@ -13,6 +19,7 @@ import (
 func Test_WithSQLite(t *testing.T) {
 	db, err := common.NewSqliteInMemory(context.Background())
 	assert.NoError(t, err)
+	defer db.Close()
 
 	_, err = db.Exec(context.Background(), `CREATE TABLE users (
     id TEXT PRIMARY KEY,
@@ -33,6 +40,51 @@ func Test_WithSQLite(t *testing.T) {
 
 	f := repository.NewFilter().AddOneToIDs(id)
 	users, err := userRepo.GetManyUsers(context.Background(), *f)
+	assert.NoError(t, err)
+	assert.Equal(t, []repository.User{*testUser}, users)
+}
+
+func Test_WithPostgres(t *testing.T) {
+	ctx := context.Background()
+
+	c, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("postgres:15.3-alpine"),
+		postgres.WithDatabase("users-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	)
+	assert.NoError(t, err)
+
+	connStr, err := c.ConnectionString(ctx, "sslmode=disable")
+	assert.NoError(t, err)
+
+	db, err := common.NewPostgres(ctx, &common.PgxConnectionProvider{connStr})
+	assert.NoError(t, err)
+
+	_, err = db.Exec(ctx, `CREATE TABLE users (
+        id UUID PRIMARY KEY,
+        name TEXT,
+        email TEXT);`)
+	if err != nil {
+		log.Fatalf("Could not create table: %v", err)
+	}
+
+	userRepo := repository.New(&db)
+
+	id := uuid.New()
+	testUser := &repository.User{ID: id, Name: "name", Email: "email"}
+	err = userRepo.CreateUser(ctx, testUser)
+	assert.NoError(t, err)
+
+	user, err := userRepo.GetUser(ctx, id)
+	assert.NoError(t, err)
+	assert.Equal(t, testUser, user)
+
+	f := repository.NewFilter().AddOneToIDs(id)
+	users, err := userRepo.GetManyUsers(ctx, *f)
 	assert.NoError(t, err)
 	assert.Equal(t, []repository.User{*testUser}, users)
 }
