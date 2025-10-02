3b935706770db4a7b5d94821d236e18012884c97
AugustineAurelius <kserbulov@gmail.com>
2025-10-02 16:00:23 +0300

    count 1

--- example/repository/user.go
+++ example/repository/user.go
@@ -1,15 +1,13 @@
 package repository
 
-import "github.com/shopspring/decimal"
-
 //go:generate go run github.com/AugustineAurelius/eos/ generator repository  --type User  --default_id=true --table=users
 type User struct {
 	ID    int
 	Name  string
 	Email *string
 	// Booler  bool
-	Balance  float64
-	Balance2 decimal.Decimal
+	Balance float64
+	// Balance2 decimal.Decimal
 	// Created time.Time
 	// Addresses []string
 	// UserTime int
